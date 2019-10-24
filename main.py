bb2fd6104b7040ea2a90390f063bdd82ddacbacb
fujii <is0232xf@gmail.com>
2019-10-24 17:03:48 +0900

    [add] save state function & draw state

--- main.py
+++ main.py
@@ -5,3 +5,23 @@ Created on Thu Oct 24 14:43:13 2019
 @author: FujiiChang
 """
 
+import csv
+
+file = open('status.csv', 'a')
+csvWriter = csv.writer(file) 
+csvWriter.writerow(['longitude', 'latitude', 'yaw'])
+
+def save_status_data(lon, lat, yaw):
+    csvWriter.wirterow([lon,  lat, yaw])
+
+def draw_status(lon, lat, yaw):
+    pass
+
+lon = 135.00
+lat = 35.00
+yaw = 0.0
+
+save_status_data(lon, lat, yaw)
+draw_status(lon, lat, yaw)
+
+
