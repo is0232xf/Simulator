43586b427da4b53bbb99e381c376f1a5906ff527
fujii <is0232xf@gmail.com>
2019-10-25 17:10:59 +0900

    [add] essential function

--- request_mav_data.py
+++ request_mav_data.py
@@ -5,3 +5,9 @@ Created on Fri Oct 25 16:54:24 2019
 @author: FujiiChang
 """
 
+from pymavlink import mavutil
+
+the_connection = mavutil.mavlink_connection('udpin:localhost:14540')
+
+the_connection.mav.request_data_stream_send(the_connection.target_system, the_connection.target_component,
+                                        mavutil.mavlink.MAV_DATA_STREAM_ALL, 1, 1)
