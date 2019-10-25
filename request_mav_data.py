1d7cd0064eb5fde189ecf1363239d90b912100ce
Yasuyuki Fujii <is0232xf@ed.ritsumei.ac.jp>
2019-10-25 17:42:18 +0900

    [add] print msg for debugging code

--- request_mav_data.py
+++ request_mav_data.py
@@ -5,9 +5,12 @@ Created on Fri Oct 25 16:54:24 2019
 @author: FujiiChang
 """
 
+import time
 from pymavlink import mavutil
 
-the_connection = mavutil.mavlink_connection('udpin:localhost:14540')
-
-the_connection.mav.request_data_stream_send(the_connection.target_system, the_connection.target_component,
+the_connection = mavutil.mavlink_connection("udpin:0.0.0.0:14551")
+print("Succeeded to connection")
+msg = the_connection.mav.request_data_stream_send(the_connection.target_system, the_connection.target_component,
                                         mavutil.mavlink.MAV_DATA_STREAM_ALL, 1, 1)
+print(msg)
+print("Finish")
