b6eb850b4d5ec7a74eeaffd02a6c526de06a0443
Yasuyuki Fujii <is0232xf@ed.ritsumei.ac.jp>
2019-10-28 18:36:33 +0900

    [delete] needlee row

--- request_mav_data.py
+++ request_mav_data.py
@@ -12,7 +12,6 @@ the_connection = connect("udpin:0.0.0.0:14551", wait_ready=True)
 print("Succeeded to connection")
 
 while True:
-	# print("CH0: %s" % the_connection.channels['0'])
 	print("CH3: %s" % the_connection.channels['3'])
 	print("CH4: %s" % the_connection.channels['4'])
 	print("CH5: %s" % the_connection.channels['5'])
