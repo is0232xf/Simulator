40e1c8f8afd675ac5ac43e75bdddbf508ea9fdd4
Yasuyuki Fujii <is0232xf@ed.ritsumei.ac.jp>
2019-10-28 18:27:44 +0900

    [add] print all thrusters pwm value

--- request_mav_data.py
+++ request_mav_data.py
@@ -6,15 +6,17 @@ Created on Fri Oct 25 16:54:24 2019
 """
 
 import time
-from pymavlink import mavutil
+from dronekit import connect
 
-the_connection = mavutil.mavlink_connection("udpin:0.0.0.0:14551")
+the_connection = connect("udpin:0.0.0.0:14551", wait_ready=True)
 print("Succeeded to connection")
 
-for channel in range(len(the_connection.channels)):
-    str_channel = str(channel)
-    print(str_channel)
-    print("%s" % the_connection.channels[str_channel])
-    time.sleep(1)
+while True:
+	# print("CH0: %s" % the_connection.channels['0'])
+	print("CH3: %s" % the_connection.channels['3'])
+	print("CH4: %s" % the_connection.channels['4'])
+	print("CH5: %s" % the_connection.channels['5'])
+	print("CH6: %s" % the_connection.channels['6'])
+	time.sleep(1)
 
 print("Finish")
