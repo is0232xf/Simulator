32dc6f64bcfca2b21cf9f1cf4fd3f941bb28f3c8
fujii <is0232xf@gmail.com>
2019-11-08 17:49:14 +0900

    [add] main function

--- controller/controller.py
+++ controller/controller.py
@@ -5,6 +5,41 @@ Created on Thu Nov  7 16:54:01 2019
 @author: FujiiChang
 """
 
-import dronekit
+import time
+import math
+import calculate_angle
+import numpy as np
+import calculate_degree as cal_deg
+from ArduPilotDriver import ArduPilotDriver
 from geopy.distance import geodesic
 
+BIWAKO = ArduPilotDriver()
+
+BIWAKO.start()
+BIWAKO.takeoff(0)
+
+way_point = np.array([34.5, 135.98], [34.5, 135.98], [34.5, 135.98])
+target_point = way_point[0]
+
+while True:
+    current_point = np.array([BIWAKO.vehicle.location.global_relative_frame.lat,
+                              BIWAKO.vehicle.location.global_relative_frame.lon])
+    current_yaw = BIWAKO.vehicle.heading
+    diff_distance = geodesic(current_point, target_point).m
+    target_direction = cal_deg.calculate_bearing(current_point, target_point)
+    if diff_distance < 2:
+        print("Keep its position")
+    else:
+        current_yaw = math.degrees(calculate_angle.limit_angle(current_yaw))
+        diff_deg = target_direction - current_yaw
+        diff_deg = math.radians(diff_deg)
+        diff_deg = math.degrees(calculate_angle.limit_angle(diff_deg))
+        if 3 > abs(diff_deg):
+            command = 's'
+        elif diff_deg >= 3:
+            command = 'l'
+        elif diff_deg < -3:
+            command = 'r'
+        command_send(command)
+        
+        
