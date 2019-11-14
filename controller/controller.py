ac5db6b599cee082e5d816b9cc003407f6e9a736
fujii <is0232xf@gmail.com>
2019-11-14 15:53:48 +0900

    [fix] controller

--- controller/controller.py
+++ controller/controller.py
@@ -19,35 +19,44 @@ BIWAKO = ArduPilotDriver()
 BIWAKO.start()
 BIWAKO.takeoff(0)
 
-way_point = np.array([[34.5, 135.98], [34.5, 135.98], [34.5, 135.98]])
+way_point = np.array([[34.980500, 135.963573], [34.981265, 135.963602], [34.980500, 135.963573]])
 way_point_num = 0
 target_point = way_point[way_point_num]
 
 while True:
     current_point = np.array([BIWAKO.vehicle.location.global_relative_frame.lat,
                               BIWAKO.vehicle.location.global_relative_frame.lon])
+    print("current point: ", current_point)
     current_yaw = BIWAKO.vehicle.heading
+    print("current yaw: ", current_yaw)
     diff_distance = geodesic(current_point, target_point).m
-    target_direction = cal_deg.calculate_bearing(current_point, target_point)
+    print("distance: ", diff_distance)
+    
+    # check distance between current and target
     if diff_distance < 2:
-        print("Keep its position")
-        time.sleep(10)
+        print("complete mission ", way_point_num)
         way_point_num = way_point_num + 1
-        print("Check its position again")
+        target_point = way_point[way_point_num]
+        print("change waypoint")
         if way_point_num < len(way_point):
             target_point = way_point[way_point_num]
         else:
             break
     else:
+        target_direction = cal_deg.calculate_bearing(current_point, target_point)
         current_yaw = math.degrees(calculate_angle.limit_angle(current_yaw))
         diff_deg = target_direction - current_yaw
         diff_deg = math.radians(diff_deg)
         diff_deg = math.degrees(calculate_angle.limit_angle(diff_deg))
+        print("diff deg: ", diff_deg)
         if 3 > abs(diff_deg):
             send_command.set_rc_channel_pwm(5, 1700)
+            print("command: straight")
         elif diff_deg >= 3:
-            send_command.set_rc_channel_pwm(4, 1700)
+            send_command.set_rc_channel_pwm(3, 1700)
+            print("command: turn Right")
         elif diff_deg < -3:
-            send_command.set_rc_channel_pwm(4, 1300)
-    time.sleep(1)
+            send_command.set_rc_channel_pwm(3, 1300)
+            print("command: turn Left")
+        time.sleep(1)
 print("Mission complete")
