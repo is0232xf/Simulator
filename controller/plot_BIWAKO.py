14c31f0acadbcc51e8e2fd439d09aa75f2b59c61
fujii <is0232xf@gmail.com>
2019-11-28 15:48:09 +0900

    [add] function reads waypoint data from csv

--- controller/plot_BIWAKO.py
+++ controller/plot_BIWAKO.py
@@ -8,28 +8,32 @@ Created on Fri Nov 15 13:20:30 2019
 import world_class
 import robot_class
 import math
+import datetime
+import os
 import numpy as np
 
+now = datetime.datetime.now()
+date = now.strftime("%Y_%m_%d_%H_%M_%S")
 
-way_point = np.array([[34.982187, 135.963695], [34.982192, 135.963596], [34.982118, 135.963598],
-                      [34.982118, 135.963698]])
-"""
-way_point = np.array([[34.980779, 135.963660], [34.981122, 135.963526], [34.981425, 135.963706], [34.981598, 135.963561],
-                      [34.981598, 135.963561], [34.981095, 135.963695],[34.980350, 135.963502]])
-"""
+os.mkdir("./plot_data/" + date)
+
+# import way_point data
+way_point = np.genfromtxt('./way_point/square.csv', delimiter=',', dtype='float', encoding='utf-8')
+print(way_point)
 world = world_class.World(way_point)
 
-data = np.genfromtxt('./csv/test6.csv', delimiter=',',
+# input tracking data 
+data = np.genfromtxt('./csv/square2.csv', delimiter=',',
                   names=True, dtype=None, encoding='utf-8')
 
 initial_pose = np.array([data[0][3], data[0][2], math.radians(data[0][4])]).T
 robot = robot_class.Robot(np.array(initial_pose))
 
 world.append(robot)
-world.draw()
+world.draw(date, 0)
 print(initial_pose)
 
 for i in range(1, len(data)):
     robot = robot_class.Robot(np.array([data[i][3], data[i][2], math.radians(data[i][4])]).T)
     world.append(robot)
-    world.draw()
+    world.draw(date, str(i))
