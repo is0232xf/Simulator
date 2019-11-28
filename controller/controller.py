7e09bb5fdb4e5822f1d24a102c89545ef2085e1c
fujii <is0232xf@gmail.com>
2019-11-28 10:51:22 +0900

    [fix] data plot simulator

--- controller/controller.py
+++ controller/controller.py
@@ -30,34 +30,27 @@ file = open('../../csv_data/'+ date +'.csv', 'a', newline='')
 csvWriter = csv.writer(file)
 # set up vehicle
 print("waiting connection")
-"""
-BIWAKO = ArduPilotDriver()
-BIWAKO.start()
-BIWAKO.takeoff(0)
+
+way_point = np.array([[34.982187, 135.963695], [34.982192, 135.963596], [34.982118, 135.963598],
+                      [34.982118, 135.963698]])
 """
 way_point = np.array([[34.980779, 135.963660], [34.981122, 135.963526], [34.981425, 135.963706], [34.981598, 135.963561],
                       [34.981598, 135.963561], [34.981095, 135.963695],[34.980350, 135.963502]])
+"""
 print(len(way_point))
 way_point_num = 0
 target_point = way_point[way_point_num]
 
-my_position = np.array([34.979778, 135.963535, math.radians(90)])
+my_position = np.array([34.982118, 135.963698, math.radians(90)])
 try:
     while True:
-        """
-        current_point = np.array([BIWAKO.vehicle.location.global_relative_frame.lat,
-                                  BIWAKO.vehicle.location.global_relative_frame.lon])
-        """
-        print("position: ", my_position)
         current_point = np.array([my_position[0], my_position[1]])
         # current_yaw = BIWAKO.vehicle.heading
         current_yaw = my_position[2]
-        #print("current yaw: ", current_yaw)
         diff_distance = geodesic(current_point, target_point).m
-        print("distance: ", diff_distance)
         
         # check distance between current and target
-        if abs(diff_distance) < 3:
+        if abs(diff_distance) < 1:
             print("complete mission ", way_point_num)
             time.sleep(2)
             way_point_num = way_point_num + 1
@@ -66,41 +59,33 @@ try:
             target_point = way_point[way_point_num]
             print("change waypoint")
             print("next way point: ", target_point)
+        # when the device has not received
         else:
-            target_direction = cal_deg.calculate_bearing(current_point, target_point)
-            # print("td: ", math.degrees(target_direction))
-            # print("target direction: ", target_direction)
+            target_direction = math.degrees(calculate_angle.limit_angle(
+                    math.radians(cal_deg.calculate_bearing(current_point, target_point))))
+            print("target deg: ", target_direction)
             current_yaw = math.degrees(current_yaw)
-            # print("yaw: ", current_yaw)
-            diff_deg = target_direction - current_yaw
+            diff_deg =  math.degrees(calculate_angle.limit_angle(math.radians(target_direction - current_yaw)))
             print("diff deg: ", diff_deg)
-            print("direction: ", my_position[2])
-            if abs(diff_deg) < 3:
-                #send_command.set_rc_channel_pwm(5, 1700)
-                #print("command: straight")
-                my_position[0] = my_position[0] + 5*ey*math.cos(my_position[2])
-                my_position[1] = my_position[1] + 5*ex*math.sin(my_position[2])
+            # when the device aims to the target point
+            if abs(diff_deg) < 2:
+                my_position[0] = my_position[0] + 2*ey*math.cos(my_position[2])
+                my_position[1] = my_position[1] + 2*ex*math.sin(my_position[2])
                 print("!!!")
     
-            elif diff_deg >= 3:
-                #send_command.set_rc_channel_pwm(3, 1300)
-                #print("command: turn Right")
-                my_position[2] = my_position[2] + math.radians(3)
+            elif diff_deg >= 2:
+                if abs(diff_deg) > 180:                  
+                    my_position[2] = my_position[2] - math.radians(2)
+                else:
+                    my_position[2] = my_position[2] + math.radians(2)
                 print("my deg: ", math.degrees(my_position[2]))
-            elif diff_deg < -3:
-                #send_command.set_rc_channel_pwm(3, 1700)
-                #print("command: turn Left")
-                my_position[2] = my_position[2] - math.radians(3)
+                
+            elif diff_deg < -2:
+                if abs(diff_deg) > 180:                  
+                    my_position[2] = my_position[2] + math.radians(2)
+                else:
+                    my_position[2] = my_position[2] - math.radians(2)
                 print("my deg: ", math.degrees(my_position[2]))
-            """
-            csvWriter.writerow([target_point[0], target_point[1], 
-                                current_point[0], current_point[1], 
-                                current_yaw, 
-                                BIWAKO.vehicle.channels['3'], 
-                                BIWAKO.vehicle.channels['4'], 
-                                BIWAKO.vehicle.channels['5'], 
-                                BIWAKO.vehicle.channels['6']])
-            """
             csvWriter.writerow([target_point[0], target_point[1], 
                                 current_point[0], current_point[1], 
                                 current_yaw])
--- controller/plot_BIWAKO.py
+++ controller/plot_BIWAKO.py
@@ -10,9 +10,16 @@ import robot_class
 import math
 import numpy as np
 
-world = world_class.World()
 
-data = np.genfromtxt('test.csv', delimiter=',',
+way_point = np.array([[34.982187, 135.963695], [34.982192, 135.963596], [34.982118, 135.963598],
+                      [34.982118, 135.963698]])
+"""
+way_point = np.array([[34.980779, 135.963660], [34.981122, 135.963526], [34.981425, 135.963706], [34.981598, 135.963561],
+                      [34.981598, 135.963561], [34.981095, 135.963695],[34.980350, 135.963502]])
+"""
+world = world_class.World(way_point)
+
+data = np.genfromtxt('./csv/test6.csv', delimiter=',',
                   names=True, dtype=None, encoding='utf-8')
 
 initial_pose = np.array([data[0][3], data[0][2], math.radians(data[0][4])]).T
