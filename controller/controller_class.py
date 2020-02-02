e694d1f9fe2a136d3adc38dca8df1e212d4627cf
fujii <is0232xf@gmail.com>
2020-02-02 16:07:17 +0900

    [add] controller class

---
+++ controller/controller_class.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  2 14:53:49 2020
+
+@author: FujiiChang
+"""
+import math
+import numpy as np
+class Controller:
+    def decide_next_action(pose, target_point):
+        # decide the next action from current robot status and the next waypoint 
+        return action
+        pass
+    
+    def update_pwm_pulse(action):
+        # update pwm pulse width for each thruster from the next action
+        return T
+        pass
