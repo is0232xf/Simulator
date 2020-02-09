f02a23b0e9cb7d09bc98e491aaf1ec582d59d008
fujii <is0232xf@gmail.com>
2020-02-09 17:11:49 +0900

    [fix] classification

--- controller/controller_class.py
+++ controller/controller_class.py
@@ -5,14 +5,105 @@ Created on Sun Feb  2 14:53:49 2020
 @author: FujiiChang
 """
 import math
+import time
 import numpy as np
+import calculate_angle
+import calculate_degree as cal_deg
+from geopy.distance import geodesic
+
 class Controller:
-    def decide_next_action(pose, target_point):
-        # decide the next action from current robot status and the next waypoint 
+    def __init__(self, way_point):
+        self.way_point = way_point
+        self.way_point_num = 0
+        self.next_goal = self.way_point[self.way_point_num]
+
+    def check_way_point(self):
+        if self.way_point_num > len(self.way_point)-2:
+            return True
+        else:
+            return False
+
+    def update_way_point(self):
+        self.way_point_num = self.way_point_num + 1
+        self.next_goal = self.way_point[self.way_point_num]
+
+    def decide_next_action(self, Okebot):
+        # decide the next action from current robot status and the next waypoint
+        current_point = np.array([Okebot.x, Okebot.y])
+        current_yaw = Okebot.yaw
+        diff_distance = geodesic(current_point, self.next_goal).m
+        print(diff_distance)
+        # check distance between current and target
+        if abs(diff_distance) < 0.5:
+            print("complete mission ", self.way_point_num)
+            action = ["s", 0]
+            self.update_way_point()
+            print("change waypoint")
+            print("next way point: ", self.next_goal)
+            time.sleep(1)
+        # when the device has not received
+        else:
+            target_direction = math.degrees(calculate_angle.limit_angle(
+                    math.radians(cal_deg.calculate_bearing(current_point, self.next_goal))))
+            current_yaw = math.degrees(current_yaw)
+            diff_deg =  target_direction - current_yaw
+ 
+            if abs(diff_deg) < 2:
+                action = ["x", 0]
+            elif diff_deg >= 2:
+                action = ["r", 0]
+            elif diff_deg < -2:
+                action = ["r", 1]
+            print("next way point: ", self.next_goal)
+        print("action: ", action)
         return action
-        pass
-    
-    def update_pwm_pulse(action):
+
+    def update_pwm_pulse(self, action):
         # update pwm pulse width for each thruster from the next action
+        T1 = 1500
+        T2 = 1500
+        T3 = 1500
+        T4 = 1500
+
+
+        if action[0] == "s" and action[1] == 0:
+            T1 = 1500
+            T2 = 1500
+            T3 = 1500
+            T4 = 1500
+        elif action[0] == "x" and action[1] == 0:
+            T1 = 1600
+            T2 = 1600
+            T3 = 1400
+            T4 = 1400
+        elif action[0] == "x" and action[1] == 1:
+            T1 = 1400
+            T2 = 1400
+            T3 = 1600
+            T4 = 1600
+        elif action[0] == "y" and action[1] == 0:
+            T1 = 1600
+            T2 = 1400
+            T3 = 1600
+            T4 = 1400
+        elif action[0] == "y" and action[1] == 1:
+            T1 = 1400
+            T2 = 1600
+            T3 = 1400
+            T4 = 1600
+        elif action[0] == "r" and action[1] == 0:
+            T1 = 1600
+            T2 = 1400
+            T3 = 1400
+            T4 = 1600
+        elif action[0] == "r" and action[1] == 1:
+            T1 = 1400
+            T2 = 1600
+            T3 = 1600
+            T4 = 1400
+
+        T = np.array([[T1],
+                      [T2],
+                      [T3],
+                      [T4]])
         return T
-        pass
--- controller/robot_class.py
+++ controller/robot_class.py
@@ -6,97 +6,79 @@ Created on Fri Nov 15 12:15:26 2019
 """
 
 import math
+import numpy as np
+from earth_class import Earth
 import matplotlib.patches as patches
-
-R = 6378137 # Earth radius WGS84
+from thruster_class import Thruster
 
 class Robot:
     def __init__(self, pose, color="black"):
+        self.r = 0.000005
+        self.color = color
         self.pose = pose
         self.x = self.pose[0]
         self.y = self.pose[1]
         self.yaw = self.pose[2]
-        self.r = 0.000005
-        self.color = color
+        self.voltage = 11.1
+        self.pwm_1 = 1500
+        self.pwm_2 = 1500
+        self.pwm_3 = 1500
+        self.pwm_4 = 1500
+        self.T1 = Thruster(self.pwm_1, self.voltage)
+        self.T2 = Thruster(self.pwm_2, self.voltage)
+        self.T3 = Thruster(self.pwm_3, self.voltage)
+        self.T4 = Thruster(self.pwm_4, self.voltage)
+        self.t1  = self.T1.calc_thrust(self.pwm_1)
+        self.t2  = self.T2.calc_thrust(self.pwm_2)
+        self.t3  = self.T3.calc_thrust(self.pwm_3)
+        self.t4  = self.T4.calc_thrust(self.pwm_4)
+
+        self.earth = Earth(self.y)
+
+    def update_state(self, disturbance):
+        trque = self.thrust()
+        yaw = self.yaw
+        self.earth.calc_ex(self.earth.ey)
+        R = np.array([[math.cos(yaw), -math.sin(yaw)],
+                      [math.sin(yaw), math.cos(yaw)]])
         
-    def move(self, power, omega):
-        """
-        ey = 360/(2*math.pi*R) # latitude: 1deg -> 1m
-        ex = 360/(2*math.pi*R*math.cos(self.pose[0]*math.pi/180)) # longitude: 1deg -> 1m
-        """
-        t_x =  math.cos(self.pose[2])*power
-        t_y = math.sin(self.pose[2])*power
-        theta = math.radians(omega)
-        print("omega: ", omega)
-        print("theta: ", theta)
-        
-        print("current pose: ", self.pose)
-        self.pose[0] = self.pose[0] + t_x
-        self.pose[1] = self.pose[1] + t_y
-        self.pose[2] = self.pose[2] + theta
-        print("move to: ", self.pose)
- 
-    def therust(case, direction):
+        F = trque - disturbance
+        F_t = np.array([[F[0][0]],
+                        [F[1][0]]])
+        F_v = np.dot(R, F_t)
+        F_x = self.earth.ex * F_v[0][0] * 0.15
+        F_y = self.earth.ey * F_v[1][0] * 0.15
+        F_r = math.radians(2) * F[2][0] * 0.3
+        self.x = self.x + F_x
+        self.y = self.y + F_y
+        self.yaw = self.yaw + F_r
+
+        self.pose[0] = self.x
+        self.pose[1] = self.y
+        self.pose[2] = self.yaw
+
+    def update_pwm_pulse(self, pwm):
+        self.pwm_1 = pwm[0][0]
+        self.pwm_2 = pwm[1][0]
+        self.pwm_3 = pwm[2][0]
+        self.pwm_4 = pwm[3][0]
+
+    def update_thrust(self):
+        self.t1 = self.T1.calc_thrust(self.pwm_1)
+        self.t2 = self.T2.calc_thrust(self.pwm_2)
+        self.t3 = self.T3.calc_thrust(self.pwm_3)
+        self.t4 = self.T4.calc_thrust(self.pwm_4)
+
+    def thrust(self):
         # 入力をx方向への直進，y方向への直進，回転にする (str型)
         # 出力はスラスト力をndarray型にする
-        
-        # T is a list that has each motor 
-        T = np.array([[0],
-                      [0],
-                      [0],
-                      [0]])
-        if case == "x":
-            if direction == 0:
-                T1.set_pwm_input(1600)
-                T2.set_pwm_input(1600)
-                T3.set_pwm_input(1400)
-                T4.set_pwm_input(1400)
-                
-            elif direction == 1:
-                T1.set_pwm_input(1400)
-                T2.set_pwm_input(1400)
-                T3.set_pwm_input(1600)
-                T4.set_pwm_input(1600) 
-            
-            
-        elif case == "y":
-            if direction == 0:
-                T1.set_pwm_input(1600)
-                T2.set_pwm_input(1400)
-                T3.set_pwm_input(1600)
-                T4.set_pwm_input(1400)
-                
-                
-            elif direction == 1:
-                T1.set_pwm_input(1400)
-                T2.set_pwm_input(1600)
-                T3.set_pwm_input(1400)
-                T4.set_pwm_input(1600) 
-            
-        elif case == "r":
-            if direction == 0:
-                T1.set_pwm_input(1600)
-                T2.set_pwm_input(1400)
-                T3.set_pwm_input(1400)
-                T4.set_pwm_input(1600)
-                
-                
-            elif direction == 1:
-                T1.set_pwm_input(1400)
-                T2.set_pwm_input(1600)
-                T3.set_pwm_input(1600)
-                T4.set_pwm_input(1400)
-    
-        thrust_1 = T1.determine_thrust()
-        thrust_2 = T2.determine_thrust()
-        thrust_3 = T3.determine_thrust()
-        thrust_4 = T4.determine_thrust()
-        
-        T = np.array([[thrust_1],
-                      [thrust_2],
-                      [thrust_3],
-                      [thrust_4]])
-    
+        self.update_thrust()
+
+        T = np.array([[self.t1],
+                      [self.t2],
+                      [self.t3],
+                      [self.t4]])
+        # T is a list that has each motor
         a = math.cos(math.radians(45))
         b = math.sin(math.radians(45))
         D = 0.25 # unit[m]
@@ -104,5 +86,12 @@ class Robot:
                         [b, -b, b, -b],
                         [D, -D, -D, D]])
         t = np.dot(phi, T)
-        print(t)
         return t
+
+    def draw(self, ax):
+        x, y, theta = self.pose
+        xn = x + self.r * math.sin(theta)
+        yn = y + self.r * math.cos(theta)
+        ax.plot([x, xn], [y, yn], color=self.color)
+        c = patches.Circle(xy=(x,y), radius=self.r, fill=False, color=self.color)
+        ax.add_patch(c)
