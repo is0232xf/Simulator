[fix] formula

simulator/robot.py
@@ -59,15 +59,16 @@ class Robot:
         R = np.array([[math.cos(yaw), -math.sin(yaw)],
                       [math.sin(yaw), math.cos(yaw)]])
         
-        F = force - disturbance
+        F = force
         F_t = np.array([[F[0][0]],
                         [F[1][0]]])
         F_v = np.dot(R, F_t)
         # 以下の3式の最終項の係数は現在てきとうに設定している
-        F_x = self.earth.ex * F_v[0][0] * 0.15
-        F_y = self.earth.ey * F_v[1][0] * 0.15
+        F_x = self.earth.ex * (F_v[0][0] - disturbance[1][0]) * 0.15 
+        F_y = self.earth.ey * (F_v[1][0] - disturbance[0][0]) * 0.15 
 
-        F_r = math.radians(2) * F[2][0]
+        F_r = math.radians(1.5) * F[2][0]
+        
         self.x = self.x + F_x
         self.y = self.y + F_y
         self.yaw = self.yaw + F_r